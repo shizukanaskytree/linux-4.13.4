5eb341b52b5b78bbb93902fbb00dbd2714003995
shizukanaskytree <shizukanaskytree@gmail.com>
2017-10-24 00:05:40 -0400

    modified: info/gc_pid_timer.h. I find that I don't set declaration of sys_start_gc_timer correctly, so I modify it. Oddly, it seems don't matter...

--- info/gc_pid_timer.h
+++ info/gc_pid_timer.h
@@ -1,3 +1,3 @@
 //wxf, 2017.10.23
-asmlinkage long sys_start_gc_timer(int gc_tid[], int nr_gc);
+asmlinkage long sys_start_gc_timer(int __user *gc_tid, int __user nr_gc);
 //asmlinkage long sys_end_gc_timer(int gc_tid[], int nr_gc);
